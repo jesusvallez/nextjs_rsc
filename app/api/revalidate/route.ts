af9a524ad13fdf7866e457b3b4052865c2ec51f7
Jesus Vallez Gomis <jesusvg91@gmail.com>
2024-05-26 10:22:19 +0200

    first commit

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,25 @@
+import { revalidateTag } from 'next/cache'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const collection =
+    request.nextUrl.searchParams.get('collection') || 'collection'
+
+  revalidateTag(collection)
+
+  return NextResponse.json(
+    {
+      revalidated: true,
+      now: Date.now(),
+      cache: 'no-store'
+    },
+    {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      }
+    }
+  )
+}
